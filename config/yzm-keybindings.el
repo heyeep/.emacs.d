f091272e39cb04be169756f527437ff219a1def7
Hiep Nguyen <yzmism@gmail.com>
2017-07-24 10:41:05 -0700

    Add keybindings

--- config/yzm-keybindings.el
+++ config/yzm-keybindings.el
@@ -5,9 +5,14 @@
 
 ;; Magit
 (global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x C-g") 'magit-status)
 
 ;; Neotree
-(global-set-key [f8] 'neotree-toggle)
+(global-set-key (kbd "<f8>") 'neotree-toggle)
+
+;; Other
+(global-set-key (kbd "<f9>") (lambda() (interactive) (find-file "~/.emacs.d/init.el")))
+(global-set-key (kbd "<f7>") (lambda() (interactive) (find-file "~/Code/flappyworld/c/flappy/scene/GameScene.cpp")))
 
 ;; Don't let osx swallow Meta key.
 (setq mac-pass-command-to-system nil)
