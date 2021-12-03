f43ebced60b377245bb438c2aa88996776113cdb
Hiep Nguyen <hiepism@gmail.com>
2021-12-03 07:27:27 -0800

    Add ruby mode

--- init.el
+++ init.el
@@ -109,6 +109,7 @@
   ("\\.lisp\\'" . +commonlisp-mode))
 (use-package hpd-lua :ensure nil)
 (use-package hpd-python :ensure nil)
+(use-package hpd-ruby :ensure nil)
 (use-package hpd-scheme :ensure nil)
 (use-package hpd-swift :ensure nil)
 (use-package hpd-web :ensure nil)
@@ -125,7 +126,7 @@
  '(custom-safe-themes
    '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "b550fc3d6f0407185ace746913449f6ed5ddc4a9f0cf3be218af4fb3127c7877" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
  '(package-selected-packages
-   '(vterm company-sourcekit flycheck-swift swift-mode flycheck-swiftlint markdown-preview-mode simple-httpd markdown-mode grip-mode auto-sudoedit go-autocomplete meghanada javadoc-lookup android-mode magit lorem-ipsum graphviz-dot-mode org-plus-contrib htmlize yaml-mode go-dlv godoctor company-go go-guru go-mode company-anaconda anaconda-mode slime-company evil-magit preview-latex auctex pdf-tools ni all-the-icons-dired slime malinka ws-butler rtags dummy-h-mode flycheck-pos-tip flycheck company-quickhelp company projectile smex ivy expand-region typescript-mode tide add-node-modules-path clang-format company-ycmd ycmd counsel solarized-theme use-package))
+   '(inf-ruby projectile-rails company-inf-ruby robe multi-term vterm company-sourcekit flycheck-swift swift-mode flycheck-swiftlint markdown-preview-mode simple-httpd markdown-mode grip-mode auto-sudoedit go-autocomplete meghanada javadoc-lookup android-mode magit lorem-ipsum graphviz-dot-mode org-plus-contrib htmlize yaml-mode go-dlv godoctor company-go go-guru go-mode company-anaconda anaconda-mode slime-company evil-magit preview-latex auctex pdf-tools ni all-the-icons-dired slime malinka ws-butler rtags dummy-h-mode flycheck-pos-tip flycheck company-quickhelp company projectile smex ivy expand-region typescript-mode tide add-node-modules-path clang-format company-ycmd ycmd counsel solarized-theme use-package))
  '(safe-local-variable-values '((geiser-scheme-implementation racket))))
 
 (custom-set-faces
---
+++ lang/hpd-ruby.el
@@ -0,0 +1,52 @@
+;;;; -*- lexical-binding: t; -*-
+
+(use-package ruby-mode
+  :ensure t
+  :mode
+  "\\.rb$\\'"
+  "\\Rakefile$\\'"
+  "\\.gemspec$\\'"
+  "\\.ru$\\'"
+  "\\Gemfile$\\'"
+  "\\.rake$\\'"
+  :interpreter "ruby"
+  :init
+  (defun hn-ruby-check-for-cocoapods ()
+    "Check if the ruby file is cocoapods.
+   - Disable flycheck for CocoaPods"
+    (when (string-match-p "^.*Podfile$" (buffer-file-name))
+      (when (bound-and-true-p flycheck-mode)
+        (flycheck-mode 0))))
+  (add-hook 'ruby-mode-hook #'hn-ruby-check-for-cocoapods))
+
+(use-package robe
+  :ensure t
+  :commands (robe-mode)
+  :init
+  (hpd/company-push-backend 'company-robe)
+  (add-hook 'ruby-mode-hook #'robe-mode))
+
+(use-package projectile-rails
+  :ensure t
+  :commands (projectile-rails-on)
+  :init
+  (add-hook 'web-mode-hook #'projectile-rails-on)
+  (add-hook 'ruby-mode-hook #'projectile-rails-on)
+  :config
+  (with-eval-after-load 'evil
+    (evil-define-key 'normal projectile-rails-mode-map
+      (kbd "gf") 'projectile-rails-goto-file-at-point)))
+
+;;;###autoload
+(defun hn-ruby-mode ()
+  "Bootstrap `hpd-ruby'."
+  (dolist (alist auto-mode-alist)
+    (when (eq (cdr alist) 'hn-ruby-mode)
+      (setf (cdr alist) 'ruby-mode)))
+  (ruby-mode))
+
+(provide 'hpd-ruby)
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars unresolved noruntime cl-functions)
+;; End:
+;;; hpd-ruby.el ends here
